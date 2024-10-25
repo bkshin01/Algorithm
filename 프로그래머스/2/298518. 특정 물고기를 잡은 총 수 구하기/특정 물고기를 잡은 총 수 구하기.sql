2dcf29479eed6ad9713dabef965a57d30623debf
b.k.shin <118626963+bkshin01@users.noreply.github.com>
2024-10-25 16:54:48 +0900

    Delete 프로그래머스/2 directory

--- 프로그래머스/2/298518. 특정 물고기를 잡은 총 수 구하기/특정 물고기를 잡은 총 수 구하기.sql
+++
@@ -1,5 +0,0 @@
--- 코드를 작성해주세요
-select count(1) FISH_COUNT
-from fish_info info
-join fish_name_info name on info.fish_type = name.fish_type
-where name.fish_name like "BASS" or name.fish_name like "SNAPPER"
