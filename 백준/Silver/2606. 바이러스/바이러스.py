b6cfad2584b3fb6403c3bfcc5ad0cd99a7e6f0e6
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-01-24 18:09:15 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,25 +1,24 @@
-n = int(input()) # 노드 개수
-v = int(input()) # 간선 개수
+import sys
+input = sys.stdin.readline
 
-# n을 고려해 그래프 준비
-graph = [[] for i in range(n+1)]
-
-# v을 고려해 그래프 채우기
-for i in range(v):
-    a, b = map(int, input().split())
-    graph[a] += [b] # graph[n] = n에 연결된 노드의 값
-    graph[b] += [a]
-
-# dfs 정의
-def dfs(graph, start):
+def DFS(start):
     stack = [start]
     visited = []
+
     while stack:
         curr = stack.pop()
         if curr not in visited:
             visited.append(curr)
-            for neighbor in graph[curr]:
+            for neighbor in graph[curr][::-1]:
                 stack.append(neighbor)
-    return visited
-    
-print(len(dfs(graph, 1)) - 1)
+    return len(visited)
+
+n = int(input())
+v = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(v):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(DFS(1)-1)
