d7a4205033669e3ef9e585517497d593e2023b1e
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-01-27 15:47:27 +0900

    [Silver II] Title: 유기농 배추, Time: 200 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,39 @@
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def BFS(x, y):
-    queue = [(x, y)]
-    while queue:
-        cx, cy = queue.pop()
+def DFS(x, y):
+    stack = [(x, y)]
+
+    while stack:
+        cx, cy = stack.pop()
         if graph[cx][cy] == 1:
             graph[cx][cy] = 0
             for i in range(4):
                 nx = cx + dx[i]
                 ny = cy + dy[i]
-                if nx<0 or nx>=m or ny<0 or ny>=n:
+                if nx<0 or nx>=n or ny<0 or ny>=m:
                     continue
-                elif graph[nx][ny] == 1:
-                    queue.append((nx, ny))
+                elif graph[nx][ny] == 0:
+                    continue
+                else:
+                    stack.append((nx, ny))
 
-t = int(input())
-for _ in range(t):
+for _ in range(int(input())):
     m, n, k = map(int, input().split())
-    graph = [[0] * n for _ in range(m)]
-    
+    graph = [[0]*m for _ in range(n)]
     for _ in range(k):
-        a, b = map(int, input().split())
-        graph[a][b] = 1
+        x, y = map(int, input().split())
+        graph[y][x] = 1
     
-    count = 0
-    for i in range(m):
-        for j in range(n):
-            if graph[i][j] == 1:
-                BFS(i, j)
-                count += 1
+    # DEBUGGING
+    # for i in graph:
+    #     print(i)
 
-    print(count)
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                DFS(i, j)
+                cnt += 1
+
+    print(cnt)
