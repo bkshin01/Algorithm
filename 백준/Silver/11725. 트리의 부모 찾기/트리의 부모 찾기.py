5b9549062d8ceb94555661f1ce447627a6f7e0a8
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-02-03 18:10:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 328 ms, Memory: 50360 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,21 +1,25 @@
 import sys
+input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 n = int(input())
 graph = [[] for _ in range(n+1)]
 parent = [0] * (n+1)
-
-for _ in range(n-1):
+for i in range(1, n):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def DFS(node):
-    for i in graph[node]: # node와 연결된 모든 노드 중에서
-        if parent[i] == 0: # 부모가 없다고 표시된 노드
-            parent[i] = node # node를 부모로 표시
-            DFS(i) # 다시 해당 노드에 대해 DFS
+# DEBUGGING
+# print(graph)
 
-DFS(1)
-for j in range(2, len(parent)):
+stack = [1]
+while stack:
+    curr = stack.pop()
+    for i in graph[curr]:
+        if parent[i] == 0:
+            parent[i] = curr
+            stack.append(i)
+
+for j in range(2, n+1):
     print(parent[j])
