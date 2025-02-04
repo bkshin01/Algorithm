910cc2d402e777f0b0dc111edb2c2b7b71bda822
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-02-04 12:16:51 +0900

    [Silver I] Title: 정수 삼각형, Time: 112 ms, Memory: 41528 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,38 +1,30 @@
-n = int(input())
-triangle = []
-for _ in range(n):
-    triangle.append(list(map(int, input().split())))
+import sys
+input = sys.stdin.readline
 
-# DEBUGGING
-# print(triangle)
+n = int(input())
+tri = []
+for _ in range(n):
+    tri.append(list(map(int, input().split())))
 
 dp = []
 for i in range(n):
     dp.append([0]*(i+1))
-dp[0][0] = triangle[0][0]
+dp[0][0] = tri[0][0]
 
 if n == 1:
-    print(triangle[0][0])
+    print(tri[0][0])
     exit()
 
-dp[1][0] = dp[0][0] + triangle[1][0]
-dp[1][1] = dp[0][0] + triangle[1][1]
-
-# DEBUGGING
-# for i in range(len(dp)):
-#     print(dp[i])
+dp[1][0] = dp[0][0] + tri[1][0]
+dp[1][1] = dp[0][0] + tri[1][1]
 
 for i in range(2, n):
     for j in range(i+1):
         if j == 0:
-            dp[i][j] = dp[i-1][j] + triangle[i][j]
+            dp[i][j] = dp[i-1][j] + tri[i][j]
         elif j == i:
-            dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+            dp[i][j] = dp[i-1][j-1] + tri[i][j]
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
-
-# DEBUGGING
-# for i in range(len(dp)):
-#     print(dp[i])
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + tri[i][j]
 
 print(max(dp[n-1]))
