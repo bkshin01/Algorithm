7c20a1905349e2c77873b32df6b4636aaf69d155
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-02-11 18:32:50 +0900

    [Silver I] Title: 스타트링크, Time: 524 ms, Memory: 139772 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,16 +1,12 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-
-def BFS(start, target):
-    queue = deque([start])
-    visited = {start:0}
-
+def BFS(f, s, g, u, d):
+    queue = deque([s])
+    visited = {s:0}
     while queue:
         curr = queue.popleft()
-        if curr == target:
-            return visited[target]
+        if curr == g:
+            return visited[g]
         for i in (curr+u, curr-d):
             if 1<=i<=f and i not in visited:
                 visited[i] = visited[curr] + 1
@@ -18,4 +14,4 @@ def BFS(start, target):
     return "use the stairs"
 
 f, s, g, u, d = map(int, input().split())
-print(BFS(s, g))
+print(BFS(f, s, g, u, d))
