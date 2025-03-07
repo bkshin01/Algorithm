30def70423db5d523e9da30acf7b1cf0d42e8121
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-03-07 11:01:34 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+truth = set(input().split()[1:])
+party = []
+
+for _ in range(m):
+    party.append(set(input().split()[1:]))
+
+for _ in range(m):
+    for p in party:
+        if p & truth:
+            truth = truth.union(p)
+
+cnt = 0
+for p in party:
+    if p & truth:
+        continue
+    cnt += 1
+
+print(cnt)
