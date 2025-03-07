5a5570028451f740b27e9979e365c36bf9ed61ad
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-03-07 11:15:35 +0900

    [Gold II] Title: 친구 네트워크, Time: 296 ms, Memory: 62452 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a != b:
+        parent[b] = a
+        number[a] += number[b]
+    return number[a]
+
+for _ in range(int(input())):
+    f = int(input())
+    parent, number = {}, {}
+    for _ in range(f):
+        a, b = input().split()
+        if a not in parent:
+            parent[a] = a
+            number[a] = 1
+        if b not in parent:
+            parent[b] = b
+            number[b] = 1
+        print(union(a, b))
