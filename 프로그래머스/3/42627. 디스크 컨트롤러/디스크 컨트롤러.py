50fbaa37735abac07a1116220472d16e304346e1
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-03-14 14:16:22 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.91 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,24 +1,20 @@
+from collections import deque
 from heapq import heappop, heappush
 
 def solution(jobs):
-    answer = 0
-    time = 0
-    n = 0  
-    start = -1
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs], key=lambda x: (x[1], x[0])))
     q = []
+    heappush(q, tasks.popleft())
+    cur, result = 0, 0
     
-    while n < len(jobs):
-        for j in jobs:
-            if start < j[0] <= time:
-                heappush(q, (j[1], j[0]))
-
-        if q:
-            cur = heappop(q)
-            start = time
-            time += cur[0]
-            answer += time - cur[1]
-            n += 1
-        else:
-            time += 1
-            
-    return answer // len(jobs)
+    while q:
+        duration, arrive = heappop(q)
+        cur = max(cur+duration, arrive+duration)
+        result += (cur - arrive)
+        
+        while tasks and tasks[0][1] <= cur:
+            heappush(q, tasks.popleft())
+        
+        if tasks and len(q) == 0:
+            heappush(q, tasks.popleft())
+    return result // len(jobs)
