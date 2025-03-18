66ec1cb445b65aa8c37d063f10795fb1e3a07f86
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-03-18 16:34:00 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,15 +2,10 @@ def solution(genres, plays):
     answer = []
     dic1={}
     dic2={}
+    
     for i, (g, p) in enumerate(zip(genres, plays)):
-        if g not in dic1:
-            dic1[g] = [(i, p)]
-        else:
-            dic1[g].append((i, p))
-        if g not in dic2:
-            dic2[g] = p
-        else:
-            dic2[g] += p
+        dic1[g] = dic1.get(g, []) + [(i, p)]
+        dic2[g] = dic2.get(g, 0) + p
     
     for (k, v) in sorted(dic2.items(), key=lambda x:x[1], reverse=True):
         for (i, p) in sorted(dic1[k], key=lambda x:x[1], reverse=True)[:2]:
