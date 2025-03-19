fdf55781dd34a1030137456985e3dbc45a68e5d1
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-03-19 09:57:54 +0900

    [level 2] Title: 피로도, Time: 68.96 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,13 @@
-answer = 0
-
 def DFS(k, cnt, dungeons, visited):
-    global answer
-    if cnt > answer:
-        answer = cnt
+    max_cnt = cnt
     for i in range(len(dungeons)):
         if k >= dungeons[i][0] and not visited[i]:
             visited[i] = True
-            DFS(k - dungeons[i][1], cnt + 1, dungeons, visited)
+            max_cnt = max(max_cnt, DFS(k - dungeons[i][1], cnt + 1, dungeons, visited))
             visited[i] = False
+    return max_cnt
 
 def solution(k, dungeons):
     n = len(dungeons)
     visited = [False] * n
-    DFS(k, 0, dungeons, visited)
-    return answer
+    return DFS(k, 0, dungeons, visited)
