7e8e47a3841b3b20bd063fb23c834d485edb2187
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-15 15:09:47 +0900

    [level 2] Title: 피로도, Time: 49.67 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,13 +1,15 @@
-def DFS(k, cnt, dungeons, visited):
-    max_cnt = cnt
-    for i in range(len(dungeons)):
-        if k >= dungeons[i][0] and not visited[i]:
-            visited[i] = True
-            max_cnt = max(max_cnt, DFS(k - dungeons[i][1], cnt + 1, dungeons, visited))
-            visited[i] = False
-    return max_cnt
+from itertools import permutations
 
 def solution(k, dungeons):
-    n = len(dungeons)
-    visited = [False] * n
-    return DFS(k, 0, dungeons, visited)
+    max_count = 0
+    for perm in permutations(dungeons, len(dungeons)):
+        cur = k
+        cnt = 0
+        for required, consumed in perm:
+            if cur >= required:
+                cur -= consumed
+                cnt += 1
+            else:
+                break
+        max_count = max(max_count, cnt)
+    return max_count
