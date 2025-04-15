8ead5bd9c5741a80b35936e99b2363880f5e033c
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-15 15:03:13 +0900

    [level 2] Title: 카펫, Time: 116.57 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,8 @@
 def solution(brown, yellow):
-    arr = []
-    for i in range(1, int(yellow**0.5)+1):
-        if yellow%i==0:
-            arr.append((i, yellow//i))
-    
-    answer = []
-    for candidate in arr:
-        a, b = candidate[0], candidate[1]
-        if (a+2)*(b+2)-yellow==brown:
-            answer.append(a+2)
-            answer.append(b+2)
-    
-    return sorted(answer, reverse=True)
+    candidates = []
+    for i in range(1, yellow+1):
+        if yellow % i == 0:
+            candidates.append((i, yellow//i))
+    for a, b in candidates:
+        if brown == (a+b)*2+4:
+            return sorted([a+2, b+2], reverse=True)
