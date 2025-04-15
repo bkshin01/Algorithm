cb95912da0ff7d1c1b2a84a4fc59f02ff0ca6c29
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-15 14:32:17 +0900

    [level 1] Title: 모의고사, Time: 2.04 ms, Memory: 9.57 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,24 +1,22 @@
 def solution(answers):
     n = len(answers)
-    s1 = [1, 2, 3, 4, 5]
-    s1 = s1*(n//5) + s1[:(n%5)]
-    s2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    s2 = s2*(n//8) + s2[:(n%8)]
-    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    s3 = s3*(n//10) + s3[:(n%10)]
-    result = [0]*(4)
+    result = [0, 0, 0]
+    s1 = [1, 2, 3, 4, 5]*(n//5) + [1, 2, 3, 4, 5][:n%5]
+    s2 = [2, 1, 2, 3, 2, 4, 2, 5]*(n//8) + [2, 1, 2, 3, 2, 4, 2, 5][:n%8]
+    s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*(n//10) + [3, 3, 1, 1, 2, 2, 4, 4, 5, 5][:n%10]
     
     for i in range(n):
-        if answers[i] == s1[i]:
-            result[1]+=1
-        if answers[i] == s2[i]:
-            result[2]+=1
-        if answers[i] == s3[i]:
-            result[3]+=1
+        a = answers[i]
+        if a == s1[i]:
+            result[0] += 1
+        if a == s2[i]:
+            result[1] += 1
+        if a == s3[i]:
+            result[2] += 1
     
     answer = []
-    for i in range(1, 4):
-        if result[i] == max(result):
-            answer.append(i)
-    
+    for i in range(3):
+        r = result[i]
+        if r == max(result):
+            answer.append(i+1)
     return answer
