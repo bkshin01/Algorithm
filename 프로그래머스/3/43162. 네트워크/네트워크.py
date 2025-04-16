c30626d55af063255244a2e0777becba4b4d1759
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-16 21:43:35 +0900

    [level 3] Title: 네트워크, Time: 2.35 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,20 +1,19 @@
+from collections import deque
+
+def BFS(start, graph, visited):
+    q = deque([start])
+    while q:
+        cur = q.popleft()
+        visited[cur] = True
+        for nxt, i in enumerate(graph[cur-1]):
+            if i == 1 and (nxt+1) != cur and not visited[nxt+1]:
+                q.append(nxt+1)
+    
 def solution(n, computers):
     answer = 0
-    visited = [False for _ in range(n)]
-    for com in range(n):
-        if visited[com] == False:
-            BFS(n, computers, com, visited)
+    visited = [False] * (n+1)
+    for com in range(1, n+1):
+        if not visited[com]:
             answer += 1
+            BFS(com, computers, visited)
     return answer
-
-def BFS(n, computers, com, visited):
-    visited[com] = True
-    queue = []
-    queue.append(com)
-    while queue:
-        com = queue.pop()
-        visited[com] = True
-        for connect in range(n):
-            if connect != com and computers[com][connect] == 1:
-                if visited[connect] == False:
-                    queue.append(connect)
