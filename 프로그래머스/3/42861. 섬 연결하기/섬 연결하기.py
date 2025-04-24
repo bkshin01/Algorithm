fae814bc61773bac138857bb7df2d6e11978e4b8
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-24 22:07:27 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,14 +1,13 @@
 def solution(n, costs):
     answer = 0
     costs.sort(key=lambda x: x[2])
-    link = set([costs[0][0]]) # 연결된 섬
+    link = set([costs[0][0]])
     
-    # kruskal 알고리즘
     while len(link) != n:
         for s, e, c in costs:
-            if s in link and e in link: # 이미 연결된 섬
+            if s in link and e in link:
                 continue
-            if s in link or e in link: # 둘 중 하나만 연결
+            if s in link or e in link:
                 link.update([s, e])
                 answer += c
                 break
