f28e56debe4cbbbc9408186fdfd62ff349930fc6
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-24 16:57:29 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 67.75 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]    
+def solution(board):
+    n = len(board)
+    def BFS(start_direction):
+        visited = [[float('inf')] * n for _ in range(n)]
+        visited[0][0] = 0
+        q = deque()
+        q.append([0, 0, 0, start_direction])
+        while q:
+            cx, cy, cc, cd = q.popleft()
+            for i in range(4):
+                nx = cx + dx[i]
+                ny = cy + dy[i]
+                if nx<0 or nx>=n or ny<0 or ny>=n:
+                    continue
+                if board[nx][ny] == 1:
+                    continue
+                else:
+                    if i == cd:
+                        nc = cc + 100
+                    else:
+                        nc = cc + 600
+                    if nc < visited[nx][ny]:
+                        visited[nx][ny] = nc
+                        q.append([nx, ny, nc, i])
+                
+        return visited[-1][-1]
+
+    return min([BFS(0), BFS(1), BFS(2), BFS(3)])
