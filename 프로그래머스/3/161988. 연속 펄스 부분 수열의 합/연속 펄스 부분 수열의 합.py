d6e5b8f406427c877d2dc0bd448ea40b6f52a6b0
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-25 09:37:00 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 322.54 ms, Memory: 30.3 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,18 +1,19 @@
 def solution(sequence):
-    max_sum = -float('inf')
-    alt_sum1 = alt_sum2 = 0
-    min1 = min2 = 0
+    answer = -float('inf')
+    psum1, psum2 = 0, 0
+    min1, min2 = 0, 0
     pulse = 1
     
     for num in sequence:
-        alt_sum1 += num * pulse
-        alt_sum2 += num * -pulse
+        psum1 += num * pulse
+        psum2 += num * -pulse
         
-        max_sum = max(max_sum, alt_sum1-min1, alt_sum2-min2)
+        answer = max(answer, psum1-min1, psum2-min2)
         
-        min1 = min(min1, alt_sum1)
-        min2 = min(min2, alt_sum2)
+        # 이전까지의 합을 선택 or 지금 숫자부터 다시 시작
+        min1 = min(min1, psum1)
+        min2 = min(min2, psum2)
         
         pulse *= -1
     
-    return max_sum
+    return answer
