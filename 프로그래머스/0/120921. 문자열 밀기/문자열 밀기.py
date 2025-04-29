5f9cb28a7637a16b31ea74ea3712ae918c63d14e
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-04-29 12:17:05 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,10 +1,9 @@
 def solution(A, B):
-    n = len(A)
-    cnt = 0
-    while True:
-        if A == B:
-            return cnt
-        elif cnt >= n:
-            return -1
-        A = A[-1] + A[:n-1]
-        cnt += 1
+    if A == B:
+        return 0
+    for i in range(len(A)):
+        tmp = A[-1]+A[:-1]
+        if tmp == B:
+            return i+1
+        A = tmp
+    return -1
