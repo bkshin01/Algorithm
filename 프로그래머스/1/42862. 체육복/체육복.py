b8ff75fb1947afb71460398e911fec708410cba4
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-06 15:33:19 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,15 +1,16 @@
 def solution(n, lost, reserve):
     l_set = set(lost)
     r_set = set(reserve)
-    lost = l_set-r_set
-    reserve = r_set-l_set
     
-    save = []
-    for l in lost:
-        if l-1 in reserve:
-            save.append(l)
-            reserve.remove(l-1)
+    lost = sorted(l_set-r_set)
+    reserve = sorted(r_set-l_set)
+    
+    for l in lost[:]:
+        if l - 1 in reserve:
+            lost.remove(l)
+            reserve.remove(l - 1)
         elif l+1 in reserve:
-            save.append(l)
-            reserve.remove(l+1)
-    return n-len(lost)+len(save)
+            lost.remove(l)
+            reserve.remove(l + 1)
+            
+    return n - len(lost)
