71818c9e34d9de98da6c15aa121f4f2ddd474db3
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-07 14:51:13 +0900

    [level 3] Title: 아이템 줍기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -2,30 +2,33 @@ from collections import deque
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
-
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    graph = [[-1 for _ in range(102)] for _ in range(102)]
-    for r in rectangle:
-        x1, y1, x2, y2 = r[0]*2, r[1]*2, r[2]*2, r[3]*2
-        for i in range(x1, x2+1):
-            for j in range(y1, y2+1):
-                if x1 < i < x2 and y1 < j < y2:
-                    graph[i][j] = 0 # 사각형 내부
-                elif graph[i][j] != 0:
-                    graph[i][j] = 1 # 테두리
+    # maps 초기화
+    maps = [[-1 for _ in range(102)] for _ in range(102)]
     
-    sx, sy, ix, iy = characterX*2, characterY*2, itemX*2, itemY*2
+    # maps 갱신
+    for r in rectangle:
+        a1, b1, a2, b2 = r[0]*2, r[1]*2, r[2]*2, r[3]*2
+        for a in range(a1, a2+1):
+            for b in range(b1, b2+1):
+                if a1<a<a2 and b1<b<b2:
+                    maps[a][b] = 1 # 내부
+                elif maps[a][b] != 1:
+                    maps[a][b] = 0 # 테두리
+    
+    # BFS 최단 거리 탐색
+    q = deque([[characterX*2, characterY*2]])
     visited = [[1 for _ in range(102)] for _ in range(102)]
-    q = deque()
-    q.append((sx, sy))
+    
     while q:
         cx, cy = q.popleft()
-        if cx == ix and cy == iy:
+        if cx == itemX*2 and cy == itemY*2:
             return visited[cx][cy] // 2
         for i in range(4):
             nx = cx + dx[i]
             ny = cy + dy[i]
-            if graph[nx][ny] == 1 and visited[nx][ny] == 1:
-                visited[nx][ny] += visited[cx][cy]
+            if maps[nx][ny] == 0 and visited[nx][ny] == 1:
+                visited[nx][ny] = visited[cx][cy] + 1
                 q.append((nx, ny))
-    return 0
+    
+    return 0       
