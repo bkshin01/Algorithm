3d70bf45688be3a43bcbfc1bb88b3b05424f751d
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-10 00:32:24 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.85 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,25 +1,24 @@
-def to_minutes(time_str):
-    h, m = map(int, time_str.split(":"))
-    return h * 60 + m
+def to_minutes(string):
+		h, m = map(int, string.split(":"))
+		return h * 60 + m
 
 def to_hhmm(minutes):
-    h = minutes // 60
-    m = minutes % 60
-    return f"{h:02d}:{m:02d}"
+		h = minutes // 60
+		m = minutes % 60
+		return f"{h:02d}:{m:02d}"
 
 def solution(n, t, m, timetable):
-    crew = sorted([to_minutes(time) for time in timetable])
-    bus_time = 9 * 60
-
-    for i in range(n):
-        count = 0
-        while crew and crew[0] <= bus_time and count < m:
-            last_person = crew.pop(0)
-            count += 1
-        bus_time += t
-    
-    last_bus = 9 * 60 + (n - 1) * t
-    if count < m:
-        return to_hhmm(last_bus)
-    else:
-        return to_hhmm(last_person - 1)
+		crew = sorted([to_minutes(time) for time in timetable], reverse=True)
+		bus_time = 9 * 60
+		
+		for i in range(n):
+				cnt = 0
+				while crew and crew[-1] <= bus_time and cnt < m:
+						last_person = crew.pop()
+						cnt += 1
+				bus_time += t
+		
+		last_bus = 9 * 60 + (n-1) * t
+		if cnt < m:
+				return to_hhmm(last_bus)
+		return to_hhmm(last_person - 1)
