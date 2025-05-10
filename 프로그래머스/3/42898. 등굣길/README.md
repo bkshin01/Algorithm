75c6eee3b87a33bf635e1a0268cc42c01abdb68d
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-10 09:39:24 +0900

    [level 3] Title: 등굣길, Time: 2.77 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/README.md
+++ 프로그래머스/3/42898. 등굣길/README.md
@@ -1,10 +1,10 @@
 # [level 3] 등굣길 - 42898 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42898) 
 
 ### 성능 요약
 
-메모리: 9.33 MB, 시간: 4.30 ms
+메모리: 9.24 MB, 시간: 2.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 09:47:48
+2025년 05월 10일 09:39:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,16 +1,20 @@
 MOD = 1000000007
 
 def solution(m, n, puddles):
-    dp = [[0]*(m+1) for _ in range(n+1)]
+    dp = [[0] * (m+1) for _ in range(n+1)]
     dp[1][1] = 1
+    
+    for y, x in puddles:
+        dp[x][y] = -1
     
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if i == 1 and j == 1:
-                continue
-            if [j, i] in puddles:
+            if dp[i][j] == -1:
                 dp[i][j] = 0
-            else:
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD
+                continue
+            if i > 1:
+                dp[i][j] += dp[i-1][j]
+            if j > 1:
+                dp[i][j] += dp[i][j-1]
                 
-    return dp[n][m]
+    return dp[n][m] % MOD
