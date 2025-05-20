1183614b7db46aec14eea7de9c7413b3fc9a10f7
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-20 21:36:34 +0900

    [level 3] Title: 최고의 집합, Time: 1.29 ms, Memory: 9.65 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,11 +1,13 @@
 def solution(n, s):
-    if n > s:
+    if s < 2 or n > s:
         return [-1]
     
-    if s % n == 0:
-        return [s//n]*n
-    else:
-        tmp = [s//n]*n
-        for i in range(s%n):
-            tmp[i] += 1
-        return sorted(tmp)
+    a = s // n
+    b = s % n
+    result = [a for _ in range(n)]
+    
+    for i in range(n):
+        if i >= n - b:
+            result[i] += 1
+    
+    return result
