6d86da9c1a05e2b173f3d22f9ce4e1d5f024b643
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-05-30 11:15:31 +0900

    [level 3] Title: 이중우선순위큐, Time: 434.00 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 16.3 MB, 시간: 258.68 ms
+메모리: 15.6 MB, 시간: 434.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 10일 09:44:43
+2025년 05월 30일 11:15:31
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,24 +1,20 @@
-from heapq import heappush, heappop
+from heapq import heappop, heappush
 
 def solution(operations):
-    Mq = []
-    mq = []
-    for operation in operations:
-        op, n = operation.split()
-        if op == "I":
-            heappush(Mq, -int(n))
-            heappush(mq, int(n))
-        elif n == "1":
-            if Mq:
-                tmp = heappop(Mq)
-                mq.remove(-1*tmp)
-        elif n == "-1":
-            if mq:
-                tmp = heappop(mq)
-                Mq.remove(-1*tmp)
-    answer = [0, 0]
-    if mq:
-        answer[0] = -1*heappop(Mq)
-        answer[1] = heappop(mq)
+    min_heap = []
+    max_heap = []
     
-    return answer
+    for oper in operations:
+        op, num = oper.split()
+        if op == "I":
+            heappush(min_heap, int(num))
+            heappush(max_heap, -1*int(num))
+        elif num == "1" and max_heap:
+            min_heap.remove(-1*heappop(max_heap))
+        elif num == "-1" and min_heap:
+            max_heap.remove(-1*heappop(min_heap))
+            
+    if max_heap and min_heap:
+        return [-1*heappop(max_heap), heappop(min_heap)]
+    else:
+        return [0, 0]
