5d45438bfa2221fb6160e9d957a68696044a4328
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-06-03 00:04:13 +0900

    [level 3] Title: 단어 변환, Time: 0.37 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 0.87 ms
+메모리: 9.26 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 07일 09:11:54
+2025년 06월 03일 00:04:12
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,27 +1,28 @@
 from collections import deque
 
-def check(word_1, word_2):
-    cnt = 0
-    for a, b in zip(word_1, word_2):
-        if a != b:
-            cnt += 1
-    if cnt == 1:
-        return True
-    return False
-
 def solution(begin, target, words):
+    
     if target not in words:
         return 0
     
-    visited = {}
-    q = deque([begin])
-    visited[begin] = 0
-
+    def check(str1, str2):
+        cnt = 0
+        for ch1, ch2 in zip(str1, str2):
+            if ch1 != ch2:
+                cnt += 1
+        if cnt == 1:
+            return True
+        return False
+    
+    q = deque()
+    q.append((begin, 0))
+    visited = {begin : True}
+    
     while q:
-        cur = q.popleft()
-        if cur == target:
-            return visited[target]
-        for nxt in [w for w in words if check(cur, w)]:
+        cur_word, cur_cnt = q.popleft()
+        if cur_word == target:
+            return cur_cnt
+        for nxt in (w for w in words if check(w, cur_word)):
             if nxt not in visited:
-                q.append(nxt)
-                visited[nxt] = visited[cur] + 1
+                visited[nxt] = True
+                q.append((nxt, cur_cnt+1))
