4be95ed5d589ecc189e2751aa5f68991f5181e3b
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-06-02 15:02:56 +0900

    [level 2] Title: 타겟 넘버, Time: 162.28 ms, Memory: 48.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,12 @@
 def solution(numbers, target):
     arr = [numbers[0], -numbers[0]]
+    
     for n in numbers[1:]:
         tmp = []
+    
         for a in arr:
             tmp.append(a+n)
             tmp.append(a-n)
         arr = tmp
     
-    answer = 0
-    for a in arr:
-        if a == target:
-            answer += 1
-    return answer
+    return arr.count(target)
