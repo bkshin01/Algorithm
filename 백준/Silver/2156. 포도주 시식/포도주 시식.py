faaf8769af6df310987feb442a39f98d917907b9
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-06-05 19:39:58 +0900

    [Silver I] Title: 포도주 시식, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,17 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+wines = [0]
 n = int(input())
-wine = [0]
 for _ in range(n):
-    wine.append(int(input()))
-dp = [0]*(n+1)
-dp[1] = wine[1]
+    wines.append(int(input()))
 
 if n == 1:
-    print(wine[1])
-elif n == 2:
-    dp[2] = wine[1] + wine[2]
-    print(dp[2])
-else:
-    dp[2] = wine[1] + wine[2]
-    for i in range(3, n+1):
-        dp[i] = max(dp[i-1], wine[i]+dp[i-2], wine[i]+wine[i-1]+dp[i-3])
-    print(dp[n])
+    print(wines[1])
+    exit()
+elif n < 3:
+    print(wines[1] + wines[2])
+    exit()
+
+dp = [0] * (n+1)
+dp[1] = wines[1]
+dp[2] = wines[1] + wines[2]
+
+for i in range(3, n+1):
+    dp[i] = max(
+        dp[i-1],
+        dp[i-2] + wines[i],
+        dp[i-3] + wines[i-1] + wines[i]
+    )
+
+print(dp[-1])
