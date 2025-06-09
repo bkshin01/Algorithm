cfccbf7a4f8de246bba72d13a74bad66697556a5
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-06-09 13:49:41 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 9.28 MB, 시간: 0.03 ms
+메모리: 9.37 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 17일 12:25:23
+2025년 06월 09일 13:49:39
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,18 +1,16 @@
 def solution(name):
     n = len(name)
-    
-    # 상하 이동
     ud_move = 0
-    dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    for ch in name:
-        ud_move += min(dic.index(ch), len(dic)-dic.index(ch))
     
-    # 좌우 이동
+    for ch in name:
+        diff = ord(ch) - ord('A')
+        ud_move += min(diff, 26 - diff)
+    
     lr_move = n - 1
     for i in range(n):
-        next = i + 1
-        while next < n and name[next] == "A":
-            next += 1
-        lr_move = min(lr_move, 2*i+(n-next), i+2*(n-next))
-        
+        nxt = i + 1
+        while nxt < n and name[nxt] == 'A':
+            nxt += 1
+        lr_move = min(lr_move, 2*i+(n-nxt), i+2*(n-nxt))
+    
     return ud_move + lr_move
