1653fb3deda87515b803979f1fd2db1808dcb40b
bkshin <118626963+bkshin01@users.noreply.github.com>
2025-08-15 09:32:53 +0900

    [Silver IV] Title: 로프, Time: 104 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -3,12 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 ropes = []
-for _ in range(n):
-    ropes.append(int(input()))
-ropes.sort(reverse=True)
-
-result = 0
 for i in range(n):
-    tmp = (i+1) * ropes[i]
-    result = max(result, tmp)
-print(result)
+    ropes.append(int(input()))
+
+if n == 1:
+    print(ropes[0])
+else:
+    ropes.sort(reverse=True)
+    result = 0
+    for i in range(n):
+        r = ropes[i]
+        result = max(result, r*(i+1))
+    print(result)
